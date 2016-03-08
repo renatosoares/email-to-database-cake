58bbe1b79ecee014e7a78f5783d74f8340b4cd7a
Renato Soares <renatosoarespro@hotmail.com>
2016-03-08 17:22:02 -0300

    estrutura do default

---
+++ theme/contato.php
@@ -0,0 +1,17 @@
+<?php 
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$telefone = $_POST['telefone'];
+$precisa = $_POST['precisa'];
+$cidade = $_POST['cidade'];
+$uf = $_POST['uf'];
+$mensagem = $_POST['mensagem'];
+
+ echo $nome "<br>";
+ echo $email "<br>";
+  echo $telefone "<br>";
+ echo $precisa "<br>";
+  echo $cidade "<br>";
+ echo $uf "<br>";
+ echo $mensagem "<br>";
+ ?>
